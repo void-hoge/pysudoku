8ca4eae7b0cfd880553664f6f9b564f3856a2eb8
void-hoge <coilgunrailgun@gmail.com>
2022-11-18 23:31:07 +0900

    remake

--- main.py
+++ main.py
@@ -1,103 +1,79 @@
 #!/usr/bin/env python3
-size = 3
 
-class board():
-	"""sudoku board class"""
-	cells = [[-1 for i in range(0, size*size)] for _ in range(0, size*size)]
-	log = []
+import sys
 
-	def __init__(self):
-		pass
+SIZE = 3
 
-	def set(self, cood, num):
-		x = cood[0]
-		y = cood[1]
-		# set number on x, y
-		self.cells[x][y] = num
-		self.log.append((cood, num))
+class sudoku:
+    def __init__(self):
+        self.cells = [[0 for _ in range(SIZE*SIZE)] for _ in range(SIZE*SIZE)]
 
-	def undo(self):
-		l = self.log.pop()
-		x = l[0][0]
-		y = l[0][1]
-		self.cells[x][y] = -1
+    def input(self):
+        raw = []
+        try:
+            while True:
+                line = input()
+                line = line.split()
+                raw+=line
+        except EOFError:
+            pass
+        for row in range(SIZE*SIZE):
+            for col in range(SIZE*SIZE):
+                try:
+                    self.cells[row][col] = int(raw[row*SIZE*SIZE+col])
+                except ValueError:
+                    self.cells[row][col] = -1
+                if self.cells[row][col] >= SIZE*SIZE:
+                    raise ValueError('Too large input cell.')
 
-	def is_blank(self, cood):
-		if self.cells[cood[0]][cood[1]] == -1:
-			return True
-		else:
-			return False
+    def print(self, ost=sys.stdout):
+        for row in range(SIZE*SIZE):
+            for col in range(SIZE*SIZE):
+                if self.cells[row][col] == -1:
+                    print('  ', file=ost, end='')
+                else:
+                    print(f'{self.cells[row][col]:1} ', file=ost, end='')
+            print('', file=ost)
+        print('', file=ost)
 
-	def get_candidate(self, cood):
-		x = cood[0]
-		y = cood[1]
-		# get candidate on x, y
-		if not(self.is_blank(cood)):
-			return set()
-		st = set()
-		# row, column
-		for i in range(size*size):
-			st.add(self.cells[x][i])
-			st.add(self.cells[i][y])
-		# block
-		blockx = x//size
-		blocky = y//size
-		for i in range(size):
-			for j in range(size):
-				st.add(self.cells[blockx*size+i][blocky*size+j])
-		return set([i for i in range(-1,size*size)]).difference(st)
+    def get_first_blank(self):
+        for row in range(SIZE*SIZE):
+            for col in range(SIZE*SIZE):
+                if self.cells[row][col] == -1:
+                    return row, col
+        return None
 
-	def get_blank(self):
-		# get first blank
-		for i in range(size*size):
-			for j in range(size*size):
-				if self.is_blank((i, j)):
-					return (i,j)
-		return None
+    def get_candidates(self, row, col):
+        hist = {i:0 for i in range(-1, SIZE*SIZE)}
+        for i in range(SIZE*SIZE):
+            hist[self.cells[row][i]] += 1
+            hist[self.cells[i][col]] += 1
+        b_row = row//SIZE
+        b_col = col//SIZE
+        for i in range(SIZE):
+            for j in range(SIZE):
+                hist[self.cells[b_row*SIZE+i][b_col*SIZE+j]] += 1
+        return [i for i in range(SIZE*SIZE) if hist[i] == 0]
 
-	def stdin(self):
-		tmp = input()
-		inlist = tmp.split()
-		# print(inlist)
-		for i in range(len(self.cells)):
-			for j in range(len(self.cells[i])):
-				if inlist[size*size*i+j] != "-":
-					self.cells[i][j] = int(inlist[size*size*i+j])
-
-	def print(self):
-		print("+-------------------+")
-		for l in self.cells:
-			print("| ", end='')
-			for c in l:
-				if c == -1:
-					print(' ', end=' ')
-				else:
-					print(c, end=' ')
-			print("|")
-		print("+-------------------+")
-
-def dfs(bd, nodecount):
-	nodecount[0]+=1
-	blank = bd.get_blank()
-	if blank == None:
-		# all blanks are filled, a solution found
-		return True
-	candidate = bd.get_candidate(blank)
-	for cnd in list(candidate):
-		bd.set(blank, cnd)
-		if dfs(bd, nodecount):
-			return True
-		bd.undo()
-	return False
+    def dfs(self):
+        blank = self.get_first_blank()
+        if not blank:
+            return True
+        row, col = blank
+        cands = self.get_candidates(row, col)
+        for num in cands:
+            self.cells[row][col] = num
+            if self.dfs():
+                return True
+            self.cells[row][col] = -1
+        return False
 
 def main():
-	bd = board()
-	bd.stdin()
-	bd.print()
-	nodecount = [0]
-	dfs(bd, nodecount)
-	bd.print()
-	print("node count: "+str(nodecount[0]))
+    sdk = sudoku()
+    sdk.input()
+    sdk.print()
+    sdk.dfs()
+    sdk.print()
 
 if __name__ == '__main__':
-	main()
+    main()
